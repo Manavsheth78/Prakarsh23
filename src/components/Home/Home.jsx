3344481446f520f1b054fc850190388ec811718c
vadhwana parth <20it75@svitvasad.ac.in>
2023-01-27 07:03:27 +0000

    Update Home.css and Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,19 +9,21 @@ const Home = () => {
           <div className="home__title">
             <h5>Welcome to prakarsh'23</h5>
           </div>
-          <div className="home__description">
-            <p>
-              Prakarsh ‘23’s vision is to assist the rural areas, and villages
-              and to encourage them in the advancement of their lives through
-              technologies developed in Prakarsh by students.{" "}
-            </p>
+          <p>
+            Prakarsh ‘23’s vision is to assist the rural areas, and villages and
+            to encourage them in the advancement of their lives through
+            technologies developed in Prakarsh by students.{" "}
+          </p>
+          <div className="glow">
+            <span class="fast-flicker">co</span>mi
+            <span class="flicker">n</span>gs<span class="flicker">oo</span>n
           </div>
           <div className="home__btn"></div>
         </div>
         <div className="home__logo">
           <div className="home__logo-img">
             <img src={IMG} alt="" />
-            <p>1-2 April</p>
+            {/* <p>1-2 April</p> */}
           </div>
         </div>
       </div>
