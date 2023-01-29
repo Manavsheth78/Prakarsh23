33d141c427c2576df96916981e10dc22fb25b43d
vadhwana parth <20it75@svitvasad.ac.in>
2023-01-29 14:32:56 +0000

    Update 5 files

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,17 +15,18 @@ const Home = () => {
             to encourage them in the advancement of their lives through
             technologies developed in Prakarsh by students.{" "}
           </p>
-          <div className="glow">
-            <span class="fast-flicker">co</span>mi
-            <span class="flicker">n</span>gs<span class="flicker">oo</span>n
-          </div>
           <div className="home__btn"></div>
         </div>
+
         <div className="home__logo">
           <div className="home__logo-img">
             <img src={IMG} alt="" />
             {/* <p>1-2 April</p> */}
           </div>
+        </div>
+        <div className="glow">
+          <span class="fast-flicker">co</span>mi
+          <span class="flicker">n</span>gs<span class="flicker">oo</span>n
         </div>
       </div>
     </section>
