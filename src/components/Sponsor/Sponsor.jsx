8d53d2c37ac848bddf6ea3a1804342330de89f01
Karan Megha <92364515+KaranMegha@users.noreply.github.com>
2023-01-29 15:23:09 +0000

    Sponsors

--- src/components/Sponsor/Sponsor.jsx
+++ src/components/Sponsor/Sponsor.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import './Sponsor.css'
 
 function Sponsor() {
   return (
-    <div>Sponsor</div>
-  )
+    <Carousel>
+      <Carousel.Item interval={2000}>
+        <img
+          className="mx-auto d-block"
+          src="assests/Sponsor/Title.svg"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img
+          className="mx-auto d-block"
+          src="assests/Sponsor/Gold.png"
+          alt="Second slide"
+        />
+  
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img
+          className="mx-auto d-block"
+          src="assests/Sponsor/Platinum.png"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img
+          className="mx-auto d-block"
+          src="assests/Sponsor/Platinum.png"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
-export default Sponsor
+export default Sponsor;
--- src/components/Team/TeamCard.jsx
+++ src/components/Team/TeamCard.jsx
@@ -4,7 +4,7 @@ import { Col } from "react-bootstrap";
 export const TeamCard = ({name, imgUrl, githublink, animation}) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
+      <div className="proj-imgbx mx-auto">
         <img src={imgUrl} alt="im"/>
         
         <div className="proj-txtx">
