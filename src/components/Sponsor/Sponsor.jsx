83a6b80c04a1200583be0003704808e512bb097a
SHETH MANAV PRASHANT <91129351+Manavsheth78@users.noreply.github.com>
2023-01-30 09:04:45 +0000

    Pending changes exported from your codespace

--- src/components/Sponsor/Sponsor.jsx
+++ src/components/Sponsor/Sponsor.jsx
@@ -12,27 +12,50 @@ function Sponsor() {
         />
       </Carousel.Item>
       <Carousel.Item interval={2000}>
-        <img
-          className="mx-auto d-block"
-          src="assests/Sponsor/Gold.png"
-          alt="Second slide"
-        />
-  
+          <img
+            className="mx-auto d-block"
+            src="assests/Sponsor/POWEREDBY-SPONSOR.png"
+            alt="Second slide"
+          />
       </Carousel.Item>
       <Carousel.Item interval={2000}>
         <img
           className="mx-auto d-block"
-          src="assests/Sponsor/Platinum.png"
+          src="assests/Sponsor/PLATINUM-SPONSOR.png"
           alt="Third slide"
         />
       </Carousel.Item>
       <Carousel.Item interval={2000}>
         <img
           className="mx-auto d-block"
-          src="assests/Sponsor/Platinum.png"
-          alt="Third slide"
+          src="assests/Sponsor/GOLD-SPONSOR.png"
+          alt="Fourth slide"
+        />
+      <Carousel.Item interval={2000}>
+        <img
+          className="mx-auto d-block"
+          src="assests/Sponsor/SILVER-SPONSOR.png"
+          alt="Fifth slide"
         />
       </Carousel.Item>
+      
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img
+          className="mx-auto d-block"
+          src="assests/Sponsor/SILVER-SPONSOR.png"
+          alt="sixth slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img
+          className="mx-auto d-block"
+          src="assests/Sponsor/BRONZE-SPONSOR.png"
+          alt="Seventh slide"
+        />
+      </Carousel.Item>
+      
+      
     </Carousel>
   );
 }
