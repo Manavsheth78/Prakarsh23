36f3cfbcf2a8da0eb5aa52b8ddc4de35154bf491
SHETH MANAV PRASHANT <shethmanav2002@gmail.com>
2023-02-01 06:56:05 +0000

    Update Footer.jsx bug solve

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,12 +11,10 @@ import { AiFillFacebook } from "react-icons/ai";
 import { BsInstagram } from "react-icons/bs";
 import belowfooterlogo from "../../assets/belowfooterlogo.png";
 
-import svit from '../../assets/svitlogo.png'
-
+import svit from "../../assets/svitlogo.png";
 
 function Footer() {
   return (
-
     <section id="footer">
       <div className="f">
         <div className="footer">
@@ -66,45 +64,8 @@ function Footer() {
             <img src={belowfooterlogo} />
           </div>
         </div>
-
-    <div className="footer">
-
-          {/* Column1 */}
-          <div className="col">
-          <img src={footer} />
-          </div>
-          {/* Column2 */}
-          <div className="right">
-          <h1>
-            <b> contact us </b>{" "}
-          </h1>
-          <div className="finfo">
-            <a href="#" className="flinks">
-              {" "}
-              <IoLocationSharp className="ficons" /> Sardar Vallabhai Patel
-              institute of Technology{" "}
-            </a>
-            <a href="#" className="flinks">
-              {" "}
-              <IoMdMail className="ficons" />
-              PRAKARSH.ORG
-            </a>
-            <a href="#" className="flinks">
-              {" "}
-              <AiFillPhone className="ficons" /> 0123456789{" "}
-            </a>
-          </div>
-
       </div>
-      <hr />
-      <div className="btm">
-      <img src={svit}/>
-      <p className="col-sm">
-             SARDAR VALLABHBHAI PATEL INSTITUTE OF TECHNOLOGY
-      </p>
-      </div>
-          
-    </div>
+    </section>
   );
 }
 
